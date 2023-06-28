159c2c4d7e7c1ba169952ba0be999b2a9427a0a4
李炬增 <97277919+lijuzeng@users.noreply.github.com>
2023-06-28 09:38:10 +0800

    Update Anuran_Calls.py

--- public_dataset/Anuran_Calls.py
+++ public_dataset/Anuran_Calls.py
@@ -3,12 +3,12 @@ import numpy as np
 
 def draw_scatter(s):
     """
-    param s : 点的大小，整数
+    param s : Scatter size
     return : None
     """
     data1 = np.loadtxt('/home/ljz/CLionProjects/npca/pc.pca', encoding='utf-8', delimiter='\t')
     data2 = np.loadtxt('/home/ljz/CLionProjects/npca/pc.npca', encoding='utf-8', delimiter='\t')
-	# 通过切片获取横坐标x1
+	# Get the abscissa x by slicing
     x1 = data1[0:672, 0]            # Leptodactylidae
     x2 = data1[673:1214, 0]         # Dendrobatidae
     x3 = data1[1215:4692, 0]        # Leptodactylidae
@@ -25,7 +25,7 @@ def draw_scatter(s):
     nx6 = data2[6866:6979, 0]       # Hylidae
     nx7 = data2[6980:7047, 0]       # Bufonidae
     nx8 = data2[7048:7195, 0]       # Hylidae
-	# 通过切片获取纵坐标R
+	# Get ordinate y by slice
     y1 = data1[0:672, 1]            # Leptodactylidae
     y2 = data1[673:1214, 1]         # Dendrobatidae
     y3 = data1[1215:4692, 1]        # Leptodactylidae
@@ -42,21 +42,21 @@ def draw_scatter(s):
     ny6 = data2[6866:6979, 1]       # Hylidae
     ny7 = data2[6980:7047, 1]       # Bufonidae
     ny8 = data2[7048:7195, 1]       # Hylidae
-	# 创建画图窗口
+	# Create a drawing window
     fig = plt.figure(figsize=(10, 5))
-	# 将画图窗口分成1行1列，选择第一块区域作子图
+	# Divide the drawing window into 1 row and 2 columns
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
-	# 设置标题
+	# set title
     ax1.set_title('pca')
     ax2.set_title('npca')
-	# 设置横坐标名称
+	# Set the abscissa name
     ax1.set_xlabel('pc1')
     ax2.set_xlabel('pc1')
-	# 设置纵坐标名称
+	# Set the ordinate name
     ax1.set_ylabel('pc2')
     ax2.set_ylabel('pc2')
-	# 画散点图
+	# Draw a scatterplot
     ax1.scatter(x1, y1, s = s, c='k', label='Leptodactylidae')
     ax1.scatter(x2, y2, s = s, c='b', label='Dendrobatidae')
     ax1.scatter(x3, y3, s = s, c='k')
@@ -73,15 +73,13 @@ def draw_scatter(s):
     ax2.scatter(nx6, ny6, s = s, c='r')
     ax2.scatter(nx7, ny7, s = s, c='g')
     ax2.scatter(nx8, ny8, s = s, c='r')
-	# 调整横坐标的上下界
-    # plt.xlim(xmax=5, xmin=-5)
-    # 加图例
+    # add legend
     ax1.legend(markerscale=6, loc='upper right', ncol=4, bbox_to_anchor=(1.9, 1.15))
-	# 显示
+	# show
     plt.show()
  
  
-# 主模块
+# main
 if __name__ == "__main__":
-	# 运行
+	# run
 	draw_scatter(s=0.5)
